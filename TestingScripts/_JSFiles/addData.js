14e94b501fc044c6e45f5e40b91778500e68afce
Gopalakrishna Holla <vgkholla@gmail.com>
2014-02-07 23:10:28 -0600

    Changed the 'end' variable

--- TestingScripts/_JSFiles/addData.js
+++ TestingScripts/_JSFiles/addData.js
@@ -1,6 +1,6 @@
 use testdb
 var start = 0;
-var end = 10;
+var end = 10000;
 people = ["Marc", "Bill", "George", "Eliot", "Matt", "Trey", "Tracy", "Greg", "Steve", "Kristina", "Katie", "Jeff"];
 for(var i=start; i<end; i++){
 
