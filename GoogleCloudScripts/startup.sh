4c28228f0137986c7d8902ced3ad9bb7144b0168
Mainak Ghosh <mghosh4@illinois.edu>
2014-11-08 11:34:39 -0600

    Fixed startup script

--- GoogleCloudScripts/startup.sh
+++ GoogleCloudScripts/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update
-apt-get install vim git
-git clone https://github.com/mghosh4/EmulabScripts
 sudo mount -o ro,noload /dev/sdb1 /mnt/
+sudo apt-get update
+sudo apt-get -y install vim git
+git clone https://github.com/mghosh4/EmulabScripts
