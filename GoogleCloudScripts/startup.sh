e9a49be6758753416ac055dd152a97f590a84e9e
Mainak Ghosh <mghosh4@illinois.edu>
2014-11-08 14:22:43 -0600

    Few more changes

--- GoogleCloudScripts/startup.sh
+++ GoogleCloudScripts/startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo mount -o ro,noload /dev/sdb1 /mnt/
 sudo apt-get update
-sudo apt-get -y install vim git
+sudo apt-get -y install vim git screen
+cd ~
 git clone https://github.com/mghosh4/EmulabScripts
