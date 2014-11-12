33475ea277a61f6b8a3d122e2f1f5c32b0bb4d18
Wenting <wwtvanessa@gmail.com>
2014-11-12 16:09:15 -0600

    Update startup.sh

--- GoogleCloudScripts/startup.sh
+++ GoogleCloudScripts/startup.sh
@@ -4,5 +4,5 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
 sudo apt-get -y install vim git screen mongodb-org-tools
-cd ~
-git clone https://github.com/mghosh4/EmulabScripts
+cd /home/wenting
+git clone https://github.com/wentingwang/EmulabScripts
