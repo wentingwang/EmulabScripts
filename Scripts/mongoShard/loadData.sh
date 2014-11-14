9e6a5de4ff609150e57235a371f40f3626300833
Wenting <wwtvanessa@gmail.com>
2014-11-13 21:24:51 -0600

    Update loadData.sh

--- Scripts/mongoShard/loadData.sh
+++ Scripts/mongoShard/loadData.sh
@@ -1,8 +1,8 @@
 source=$1
 count=$2
 i=1
-QUERY_ROUTER_STRING="EmulabScripts/TestingScripts/_Runners/addDataParallelAmazon.sh"
-for  node in ${NEW_QUERY_ROUTERS//,/ }
+QUERY_ROUTER_STRING="/home/wenting/EmulabScripts/TestingScripts/_Runners/addDataParallelAmazon.sh"
+for  node in ${QUERY_ROUTERS//,/ }
 do
         echo "loading data to $node ... $QUERY_ROUTER_STRING $i $((i+count-1))"
         ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $node "
