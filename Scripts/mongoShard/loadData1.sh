e9ebb99c953f676e3006169a8e3ee29f9277292a
Wenting <wwtvanessa@gmail.com>
2014-12-13 13:37:46 -0600

    Update loadData1.sh

--- Scripts/mongoShard/loadData1.sh
+++ Scripts/mongoShard/loadData1.sh
@@ -1,10 +1,13 @@
 source $1
 
-QUERY_ROUTER_STRING="/mnt/mongo/mongo --host localhost --port 27018 < /home/wenting/EmulabScripts/TestingScripts/_JSFiles /generateData.js"
+QUERY_ROUTER_STRING="/mnt/wmongo/mongo --host localhost --port 27018 < /home/wenting/EmulabScripts/TestingScripts/_JSFiles /generateData.js"
 GIT_STRING="git clone https://github.com/wentingwang/EmulabScripts.git"
 for  node in ${QUERY_ROUTERS//,/ }
 do
-        echo "loading data to $node ... $GIT_STRING"
+        echo "clone git hub to $node ... $GIT_STRING"
+        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $node "
+            $GIT_STRING" &
+        echo "loading data to $node ... $QUERY_ROUTER_STRING"
         ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $node "
             $QUERY_ROUTER_STRING" &
 done
