e6426776f1c7b06f5bbacb2e4fc10aada5fe4181
Wenting <wwtvanessa@gmail.com>
2014-12-13 14:17:05 -0600

    Create gitClone.sh

---
+++ Scripts/mongoShard/gitClone.sh
@@ -0,0 +1,14 @@
+source $1
+
+GIT_STRING="git clone http://github.com/wentingwang/EmulabScripts"
+REMOVE_STRING="sudo rm -rf EmulabScripts/"
+for  node in ${QUERY_ROUTERS//,/ }
+do
+        echo "clone git hub to $node ... $REMOVE_STRING"
+        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $node "
+            $REMOVE_STRING" &
+         echo "clone git hub to $node ... $GIT_STRING"
+        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $node "
+            $GIT_STRING" &
+done
+echo ""
