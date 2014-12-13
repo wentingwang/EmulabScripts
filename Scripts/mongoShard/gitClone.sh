2d2f835cb74f5801f48f265c30f0f89cc2e0f03b
wentingwang <wwtvanessa@gmail.com>
2014-12-13 20:48:53 +0000

    change script

--- Scripts/mongoShard/gitClone.sh
+++ Scripts/mongoShard/gitClone.sh
@@ -1,14 +1,15 @@
 source $1
 
 GIT_STRING="git clone http://github.com/wentingwang/EmulabScripts"
-REMOVE_STRING="sudo rm -rf EmulabScripts/"
+REMOVE_STRING="sudo rm -rf /home/wenting/EmulabScripts/"
 for  node in ${QUERY_ROUTERS//,/ }
 do
         echo "clone git hub to $node ... $REMOVE_STRING"
         ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $node "
-            $REMOVE_STRING" &
+            $REMOVE_STRING"
          echo "clone git hub to $node ... $GIT_STRING"
         ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $node "
-            $GIT_STRING" &
+            cd /home/wenting &&
+            $GIT_STRING" 
 done
 echo ""
