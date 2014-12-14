70d597bfb09a5cae1ac013b1011e9d4187985cd9
Wenting <wwtvanessa@gmail.com>
2014-12-13 18:26:10 -0600

    Update loadData1.sh

--- Scripts/mongoShard/loadData1.sh
+++ Scripts/mongoShard/loadData1.sh
@@ -1,6 +1,6 @@
 source $1
 
-QUERY_ROUTER_STRING="/mnt/wmongo/mongo --host localhost --port 27017 < /home/wenting/EmulabScripts/TestingScripts/_JSFiles/generateData.js"
+QUERY_ROUTER_STRING="/mnt/wmongo/mongo --host localhost --port 27017 < /home/wenting/EmulabScripts/TestingScripts/_JSFiles/$2"
 for  node in ${QUERY_ROUTERS//,/ }
 do
         echo "loading data to $node ... $QUERY_ROUTER_STRING"
