059ac96cb7df3bdf01c2e722f22868fe3246bae5
Wenting <wwtvanessa@gmail.com>
2015-04-03 12:05:46 -0500

    Update nativeReconfig.sh
    add drop_collection and reshard js

--- Scripts/mongoShard/nativeReconfig.sh
+++ Scripts/mongoShard/nativeReconfig.sh
@@ -1,1 +1,10 @@
+source $1
+mongoexport --host $QUERY_ROUTERS  --db amazondb --collection review_collection --out temp.json
 
+DELETE_COLLECTION = "use amazondb${RETURN} db.review_collection.drop()${RETURN}"
+echo "$DELETE_COLLECTION" > drop_colleciton.js
+
+RESHARD = "sh.shardCollection\(\"amazondb.review_collection\",{user_id:1}\)"
+echo "$RESHARD" > reshard.js
+
+mongo --host $QUERY_ROUTERS < drop_collection.js
