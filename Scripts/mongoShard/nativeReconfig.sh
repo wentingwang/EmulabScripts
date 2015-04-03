83498e4313c432865e3b21af562e4608aa22f054
Wenting Wang <wwang84@node-9.mongodb-wenting.iss.emulab.net>
2015-04-03 11:23:42 -0600

    change echo and js

--- Scripts/mongoShard/nativeReconfig.sh
+++ Scripts/mongoShard/nativeReconfig.sh
@@ -1,10 +1,12 @@
-source $1
+#! /bin/bash
+source "$1"
+echo $1
 mongoexport --host $QUERY_ROUTERS  --db amazondb --collection review_collection --out temp.json
 
-DELETE_COLLECTION = "use amazondb${RETURN} db.review_collection.drop()${RETURN}"
-echo "$DELETE_COLLECTION" > drop_colleciton.js
+DELETE_COLLECTION="use amazondb\n db.review_collection.drop()"
+echo -e "$DELETE_COLLECTION" > drop_collection.js
 
-RESHARD = "sh.shardCollection\(\"amazondb.review_collection\",{user_id:1}\)"
-echo "$RESHARD" > reshard.js
+RESHARD="sh.enableSharding(\"amazondb\")\ndb.review_collection.ensureIndex({number:1})\nsh.shardCollection(\"amazondb.review_collection\",{number:1})"
+echo -e "$RESHARD" > reshard.js
 
 mongo --host $QUERY_ROUTERS < drop_collection.js
