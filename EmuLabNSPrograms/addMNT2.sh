b7bb062bd4a5fe20c35fb7d6dfdc6595f7c364c1
Wenting Wang <wwang84@node-1.sql.dcsq.emulab.net>
2015-04-15 11:49:00 -0600

    add conf for cs511

--- EmuLabNSPrograms/addMNT2.sh
+++ EmuLabNSPrograms/addMNT2.sh
@@ -2,5 +2,5 @@
 for (( i=0; i<=$1; i++ ))
 do
     echo "Setting up node-$i.mongotwo.iss.emulab.net"
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no node-$i.graphlab.dcsq.emulab.net "sudo /usr/testbed/bin/mkextrafs /mnt"
+	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no node-$i.sql.dcsq.emulab.net "sudo /usr/testbed/bin/mkextrafs /mnt"
 done
