d2880f541d22bd61378638e9d67a4824328a38f2
Wenting <wwtvanessa@gmail.com>
2015-04-15 15:46:05 -0500

    Update nativeReconfig.sh

--- Scripts/mongoShard/nativeReconfig.sh
+++ Scripts/mongoShard/nativeReconfig.sh
@@ -4,7 +4,7 @@ echo $1
 export_start=$(date +%s.%N)
 mongoexport --host $QUERY_ROUTERS  --db amazondb --collection review_collection --out /mnt/temp.json
 export_end=$(date +%s.%N)
-runtime=$((export_end-export_start))
+runtime=$(echo "$export_end-$export_start" | bc)
 echo "export time=$runtime"
 
 DELETE_COLLECTION="use amazondb\n db.review_collection.drop()"
@@ -16,17 +16,17 @@ echo -e "$RESHARD" > reshard.js
 delete_start=$(date +%s.%N)
 mongo --host $QUERY_ROUTERS < drop_collection.js
 delete_end=$(date +%s.%N)
-runtime=$((delete_end-delete_start))
+runtime=$(echo "$delete_end-$delete_start" |bc)
 echo "delete time=$runtime"
 
 reshard_start=$(date +%s.%N)
 mongo --host $QUERY_ROUTERS < reshard.js
 reshard_end=$(date +%s.%N)
-runtime=$((reshard_end-reshard_start))
+runtime=$(echo "$reshard_end-$reshard_start" | bc)
 echo "reshard time=$runtime"
 
 import_start=$(date +%s.%N)
 mongoimport --host $QUERY_ROUTERS --db amazondb --collection review_collection --file /mnt/temp.json
 import_end=$(date +%s.%N)
-runtime=$((import_end-import_start))
+runtime=$(echo "$import_end-$import_start" | bc)
 echo "import time=$runtime"
